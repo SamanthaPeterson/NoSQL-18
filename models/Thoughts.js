8d60deb6220de504706159fad1a0840dd0189831
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-10-21 19:26:17 -0600

    i am confused about current error

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,6 +1,6 @@
 // Require Mongoos and Moment
 const { Schema, model, Types } = require('mongoose');
-const moment = require('moment');
+//const moment = require('moment');
 
 // ReactionsSchema
 const ReactionsSchema = new Schema(
@@ -22,7 +22,7 @@ const ReactionsSchema = new Schema(
     createdAt: {
         type: Date,
         default: Date.now,
-        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        //get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
     }
     },
     {
