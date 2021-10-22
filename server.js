7d69c4a13f2472f3346dae83cae623b8b05e9117
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-10-21 21:12:52 -0600

    working on server js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const db = require('./models');
+const {
+  User
+} = require('./models');
 
 app.use(express.urlencoded({
   extended: true
@@ -20,5 +23,29 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network'
 mongoose.set('useCreateIndex', true);
 mongoose.set('debug', true);
 
+
+app.post('/submit', ({
+  body
+}, res) => {
+  const user = new User(body);
+
+  User.create(user)
+    .then(dbUser => {
+      res.json(dbUser);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.get('/users', (req, res) => {
+  User.find({}).then(users => {
+    res.json(users);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
 
 
