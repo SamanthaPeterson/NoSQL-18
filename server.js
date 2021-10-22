98829514706bc3e936976de05558da0965ec07cd
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-10-21 21:22:40 -0600

    working on server js something not working

--- server.js
+++ server.js
@@ -20,8 +20,12 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network'
   useUnifiedTopology: true
 });
 
-mongoose.set('useCreateIndex', true);
-mongoose.set('debug', true);
+// mongoose.set('useCreateIndex', true);
+// mongoose.set('debug', true);
+
+// mongoose.set('debug', true);
+
+// app.listen(PORT, () => console.log(` Connected on localhost:${PORT}`));
 
 
 app.post('/submit', ({
