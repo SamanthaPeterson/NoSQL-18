74b3cfb6ac51e7888da9cb5f05a4708a9868d4b6
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-10-29 08:59:35 -0600

    getting an error on index.js

--- models/index.js
+++ models/index.js
@@ -1,9 +1,8 @@
 // Require Users and Thoughts Models
 const User = require('./User');
 const Thoughts = require('./Thoughts');
-
+const Comment = require('./Comment');
 
 // Export Modules: Users and Thoughts
-module.exports = { User, Thoughts };
+module.exports = { User, Thoughts, Comments };
 
-//looks good
